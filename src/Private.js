2c0c3dddadaa298080607a0b7b4f67cf2be21dc1
Sky <mirrorcoloured@hotmail.com>
2018-11-05 20:00:49 +0000

    Delete Private.js

--- src/Private.js
+++
@@ -1,7 +0,0 @@
-// Endpoints to LearnChemE@gmail.com spreadsheets
-export let gradecatcherURL = "https://script.google.com/macros/s/AKfycbzFyWWUzGbucE6zlFRtaU__iImClayuS_i4cLQY/exec";
-export let feedbackcatcherURL = "https://script.google.com/macros/s/AKfycbzrf2_4peZHz_xVyNmHK9zBuF4U5qzdPB6sV2nqKg/exec";
-
-// Endpoints to mirrorcoloured@gmail.com spreadsheets
-export let oldgradecatcherURL = "https://script.google.com/macros/s/AKfycbzNPmE7Qx1mLXdwIvP8FyWVyDdR8FQ-ymkAFyiNcF4QC4zvVwM/exec";
-export let oldfeedbackcatcherURL = "https://script.google.com/macros/s/AKfycbyKAKkuvF87WdWUvhHbhbXvjqz3d0qBST7eJIzOTPkNhw9qKuOg/exec";
